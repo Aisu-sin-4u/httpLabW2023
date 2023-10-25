37339f64fa60ad00bd178607689c4723b4ba3d3b
Aisu-sin-4u <singhg5@spu.edu>
2023-10-24 18:20:08 -0700

    Final Submit

---
+++ http.js
@@ -0,0 +1,69 @@
+export class HTTPClient {
+    
+  //GET method
+  get(url) {
+    return new Promise(async (resolve, reject) =>{
+      let currentMethod = {
+        method: "GET"
+      }
+      const response = await fetch(url, currentMethod);
+      const data = await response.json();
+      if(response.ok){
+        resolve(JSON.stringify(data));
+      }else{
+        reject(`Get Error: ${response.status}:${response.statusText}`);
+      }
+    })
+  }
+  
+  //POST method    
+  post(url, data) {
+        return new Promise(async (resolve, reject) =>{
+          let currentMethod = {
+            method: "POST",
+            body: JSON.stringify(data)
+          }
+          const response = await fetch(url, currentMethod);
+          const output = await response.json();
+          if(response.ok){
+            resolve(JSON.stringify(output));
+          }else{
+            reject(`Post Error: ${response.status}:${response.statusText}`);
+          }
+        })
+  }
+      
+  //PUT Method    
+  put(url, data) {
+            return new Promise(async (resolve, reject) =>{
+              let currentMethod = {
+                method: "PUT",
+                body: JSON.stringify(data)
+              }
+              const response = await fetch(url, currentMethod);
+              const output = await response.json();
+              if(response.ok){
+                resolve(JSON.stringify(output));
+              }else{
+                reject(`Put Error: ${response.status}:${response.statusText}`);
+              }
+            })
+  }
+      
+  //DELETE Method    
+  delete(url) {
+          return new Promise(async (resolve, reject) =>{
+            let currentMethod = {
+              method: "DELETE",
+            }
+            const response = await fetch(url, currentMethod);
+            const output = await response.json();
+            if(response.ok){
+              resolve(JSON.stringify(output));
+            }else{
+              reject(`delete Error: ${response.status}:${response.statusText}`);
+            }
+          })
+      }
+}
+    
